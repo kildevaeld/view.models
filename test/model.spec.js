693d508380da803b39e749828d74baa77668ff58
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-07-08 14:02:56 +0200

    adds test cases

---
+++ test/model.spec.js
@@ -0,0 +1,12 @@
+describe('Model', () => {
+
+
+    it('should instantiate', () => {});
+
+    it('should set property', () => {});
+
+    it('should get property', () => {});
+
+    it('should unset property', () => {});
+
+});
